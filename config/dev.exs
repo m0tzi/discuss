5b4295aec1aa187998f343345d52f204f29b2aee
David Motz <davidmotz@Davids-MacBook-Pro.local>
2018-11-03 15:07:08 +0100

    Repair config file, add Plug Cowboy dependency

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "davidmotz",
+  password: "",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
